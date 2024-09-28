3403a788226279c757fa2ee5b8480703eefb2227
“hamo4651” <hamo4651@gmail.com>
2024-09-28 22:03:03 +0300

    login_regester_owner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
+import { PropertiesComponent } from './properties/properties.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
-        path: "search-result/:",
+        path: 'login',
+        component:LoginComponent
+    
+    },
+    {
+        path:'register',
+        component:RegisterComponent
+    },
+    {
+        path:'properties',
+        component:PropertiesComponent
     }
 ];
