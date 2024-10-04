e0a036c984b3d85efed22253a4714bc53ba6c042
MHJaafar9869 <mostafajaafar9869@gmail.com>
2024-10-04 22:02:14 +0300

    changing add-property form to be multi page form using Stepper.

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,64 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   ReactiveFormsModule,
   FormBuilder,
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { NgIf } from '@angular/common';
-import { Router } from '@angular/router';
-import { PropertyService } from '../../services/propertyService/property.service';
+import { CommonModule } from '@angular/common';
+import { CdkStep, CdkStepper } from '@angular/cdk/stepper';
+import { StepperComponent } from "../stepper/stepper.component";
+import { InformationComponent } from "./information/information.component";
+import { AmenitiesComponent } from "./amenities/amenities.component";
+import { PicturesComponent } from "./pictures/pictures.component";
+import { FinishComponent } from "./finish/finish.component";
 
 @Component({
   selector: 'app-add-property',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, CommonModule, StepperComponent, CdkStepper, CdkStep, InformationComponent, AmenitiesComponent, PicturesComponent, FinishComponent],
   templateUrl: './add-property.component.html',
   styleUrl: './add-property.component.css'
 })
 export class AddPropertyComponent {
-  AddForm: FormGroup;
-  categories: any[] = [];
-  submitted = false
 
-  constructor(private fb: FormBuilder, private router: Router, private PropertyService: PropertyService) {
-    this.AddForm = this.fb.group({
-      name: ['', Validators.required],
-      image: ['', [Validators.required,]],
-      description: ['', [Validators.required,]],
-      category_id: ['', [Validators.required,]],
-      amenities: ['', [Validators.required,]],
-      number_of_rooms: ['', [Validators.required,]],
-      headline: ['', [Validators.required,]],
-      night_rate: ['', [Validators.required,]],
-      city: ['', [Validators.required,]],
-      country: ['', [Validators.required,]],
-      address: ['', [Validators.required,]],
-      status: ['', [Validators.required,]],
-    });
-  }
-  onFileChange(event: any): void {
-    const file = event.target.files[0];
-    this.AddForm.patchValue({
-      image: file
-    });
-  }
-
-  handleSubmit() {
-    this.submitted = true;
-    if (this.AddForm.valid) {
-      const formData = new FormData();
-      Object.keys(this.AddForm.value).forEach(key => {
-        formData.append(key, this.AddForm.get(key)?.value);
-      });
-      this.PropertyService.addProperty(formData).subscribe(
-        response => {
-          console.log('property added successfully:', response);
-        },
-        error => {
-          console.error("Error Property wasn't added:", error);
-        }
-      );
-    }
-  }
 }
