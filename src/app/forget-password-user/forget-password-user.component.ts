5a5264e48b11843e2e5d34809a17789295de277e
“hamo4651” <hamo4651@gmail.com>
2024-10-06 12:22:29 +0300

    improve validation

--- src/app/forget-password-user/forget-password-user.component.ts
+++ src/app/forget-password-user/forget-password-user.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ForgetUserPasswordService } from '../services/forget-user-password.service';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-forget-password-user',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NgIf],
   templateUrl: './forget-password-user.component.html',
   styleUrl: './forget-password-user.component.css'
 })
@@ -15,7 +16,8 @@ export class ForgetPasswordUserComponent {
 
   submitted = false;
   user: any;
- 
+  errorMessage: string = '';
+
   handleSubmit(form:NgForm){
 
     this.submitted = true;
@@ -25,10 +27,21 @@ export class ForgetPasswordUserComponent {
     
         return this.authService.forgetPassword(form.value.email).subscribe((data) => {
           console.log(data);
-        });  
+          this.errorMessage = data.message;
+
+        },(error) => {
+          if (error.status === 400) {
+            this.errorMessage = 'No user found with that email';
+            console.log(this.errorMessage);
+            
+          } else {
+            this.errorMessage = 'An unexpected error occurred';
+          }        }); 
+ 
 
   }
 
 
   }
+
 
