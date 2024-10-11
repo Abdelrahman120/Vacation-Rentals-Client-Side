a9425de41e4e77076aacde91a9461a300dd25887
Abdelrahman120 <abdoahm928@gmail.com>
2024-10-11 23:21:12 +0300

    enhance owner & user details

--- src/app/owner-details/owner-details.component.ts
+++ src/app/owner-details/owner-details.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 import { AdminServicesService } from '../services/admin-services.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TruncatePipe } from '../pipes/truncate.pipe';
 
 @Component({
   selector: 'app-owner-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TruncatePipe],
   templateUrl: './owner-details.component.html',
-  styleUrl: './owner-details.component.css'
+  styleUrl: './owner-details.component.css',
 })
 export class OwnerDetailsComponent {
   owner: any;
   loading = true;
-  constructor (private adminServices : AdminServicesService , private route:ActivatedRoute) {}
+  constructor(
+    private adminServices: AdminServicesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    const ownerId : any = this.route.snapshot.paramMap.get('id');
+    const ownerId: any = this.route.snapshot.paramMap.get('id');
     this.adminServices.getOwnerDetails(ownerId).subscribe(
       (data) => {
         this.owner = data.owner;
