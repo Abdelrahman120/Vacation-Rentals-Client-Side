bb7f93cb8881a615afb5fbd7d9daad31fd36f64a
MHJaafar9869 <mostafajaafar9869@gmail.com>
2024-10-11 17:23:11 +0300

    UI improvement

--- src/app/user-bookings/item-card/item-card.component.ts
+++ src/app/user-bookings/item-card/item-card.component.ts
@@ -15,10 +15,17 @@ export class ItemCardComponent implements OnInit {
   @Input() userInfo!: UserInfo;
   @Input() property!: Property;
   @Input() payment!: Payment;
+  start_date: Date = new Date();
+  end_date: Date = new Date();
+  diffInDays: any;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.start_date = new Date(this.payment.start_date);
+    this.end_date = new Date(this.payment.end_date);
+    let diffInHours = this.end_date.getTime() - this.start_date.getTime();
+    this.diffInDays = Math.ceil(diffInHours / (1000 * 60 * 60 * 24));
     console.log('Payment:', this.payment);
     console.log('Property:', this.property);
     console.log('UserInfo:', this.userInfo);
