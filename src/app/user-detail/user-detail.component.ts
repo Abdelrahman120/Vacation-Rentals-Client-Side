c1f745b559290e212ce69ff39d48f9f0b266f517
MHJaafar9869 <mostafajaafar9869@gmail.com>
2024-10-11 14:44:59 +0300

    UI improvement

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -6,32 +6,35 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CommonModule , RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.css'
+  styleUrl: './user-detail.component.css',
 })
 export class UserDetailComponent {
-  user: any; // You can create a specific interface for user if desired
+  user: any;
   loading: boolean = true;
 
-  constructor(private userService: UserDetailsService, private route: ActivatedRoute) {}
+  constructor(
+    private userService: UserDetailsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    const userId = Number(this.route.snapshot.paramMap.get('id')); // Get user ID from the route
+    const userId = Number(this.route.snapshot.paramMap.get('id'));
     this.getUserDetails(userId);
   }
 
   getUserDetails(id: number): void {
     this.userService.getUserById(id).subscribe(
       (response) => {
-        this.user = response.data; 
+        this.user = response.data;
         console.log(response);
-        
-        this.loading = false; 
+
+        this.loading = false;
       },
       (error) => {
         console.error('Error fetching user details:', error);
-        this.loading = false; // Set loading to false on error
+        this.loading = false;
       }
     );
   }
